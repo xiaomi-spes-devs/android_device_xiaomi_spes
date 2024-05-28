17bca70bbbc54b1cbc65e842928aae58c40b5641
basamaryan <basam.aryan@gmail.com>
2024-05-29 02:11:18 +0530

    spes: Switch to the pixel power-libperfmgr
    Change-Id: Ic018a0151f7156eab2bb983e3de52cd7b8756710

--- device.mk
+++ device.mk
@@ -333,7 +333,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3.vendor \
-    android.hardware.power-service.lineage-libperfmgr \
+    android.hardware.power-service.pixel-libperfmgr \
     libqti-perfd-client
 
 # Perf
@@ -376,7 +376,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
