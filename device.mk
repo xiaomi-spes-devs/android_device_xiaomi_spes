0974e2b87e31bc7ce02b26b70060af5254ee067f
Aeoniixx <aeoniixx@gmail.com>
2025-03-15 20:41:56 +0000

    spes: Update boot-image-profile.txt path for A15 QPR2
    * frameworks/base/config/boot-image-profile.txt now moved to frameworks/base/boot/boot-image-profile.txt
    
    Change-Id: I61af081b402f6e63722a5ae9e22151227a713c95
    Signed-off-by: Aeoniixx <aeoniixx@gmail.com>
    Signed-off-by: An. <noegrahachan@gmail.com>
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- device.mk
+++ device.mk
@@ -262,7 +262,7 @@ PRODUCT_COPY_FILES += \
 
 # Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 # IRQ
 PRODUCT_COPY_FILES += \
