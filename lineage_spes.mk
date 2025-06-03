034f3a17ddff095bf739563455b85b38e95e03dd
angelomds42 <angelomds42@gmail.com>
2025-06-03 11:53:07 -0300

    spes: Update blobs from spesn TKQ1.221114.001-V816.0.11.0.TGKMIXM-release-keys
    Change-Id: I39facedb0cb7700066b98961a5884db922092240
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -28,5 +28,5 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="spes_global-user 13 TKQ1.221114.001 V816.0.8.0.TGCMIXM release-keys" \
-    BuildFingerprint=Redmi/spes_global/spes:13/TKQ1.221114.001/V816.0.8.0.TGCMIXM:user/release-keys
+    BuildDesc="spes_global-user 13 TKQ1.221114.001 V816.0.11.0.TGKMIXM release-keys" \
+    BuildFingerprint=Redmi/spes_global/spes:13/TKQ1.221114.001/V816.0.11.0.TGKMIXM:user/release-keys
