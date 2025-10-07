b70df2ab6957f03ff696f54170f9250eb5b5e731
angelomds42 <angelomds42@gmail.com>
2025-10-07 07:49:17 -0300

    fixup! spes: Define charging control path for Lineage Health HAL
    Use appropriate paths for this device
    
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- device.mk
+++ device.mk
@@ -277,7 +277,9 @@ PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
 $(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/qcom-battery/input_suspend)
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 
 # Media
 PRODUCT_PACKAGES += \
