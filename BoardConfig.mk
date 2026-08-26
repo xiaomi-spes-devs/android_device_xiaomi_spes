6dd0c471c7d465300929851830d7962627e37b6e
Angaddeep Singh <angxddeep@gmail.com>
2026-08-26 17:23:52 +0530

    spes: Drop asano.config as its now merged in spes.config
    Change-Id: I39475c8621b463a1d730b8df1b4dfdf70deb74d1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,10 +103,12 @@ BOARD_KERNEL_CMDLINE +=  init.is_dt2w_sensor=1
 BOARD_KERNEL_CMDLINE +=  init.is_st2w_sensor=1
 
 TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_CONFIG := vendor/bengal-perf_defconfig asano.config vendor/xiaomi/spes.config
 TARGET_KERNEL_SOURCE := kernel/xiaomi/spes
 TARGET_KERNEL_NO_GCC := true
 TARGET_LINUX_KERNEL_VERSION := 4.19
+TARGET_KERNEL_CONFIG := \
+	vendor/bengal-perf_defconfig \
+	vendor/xiaomi/spes.config
 
 # LMKD
 TARGET_LMKD_STATS_LOG := true
