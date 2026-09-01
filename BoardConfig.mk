afd8bac1bd7013ccdc17ac9461e911c99b76325e
Michael Bestas <mkbestas@lineageos.org>
2026-09-01 22:43:37 +0530

    spes: Drop no longer used TARGET_KERNEL_NO_GCC
    Change-Id: I416b9159e123b05720647d62f99011d8a41fbc05

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,7 +104,6 @@ BOARD_KERNEL_CMDLINE +=  init.is_st2w_sensor=1
 
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/spes
-TARGET_KERNEL_NO_GCC := true
 TARGET_LINUX_KERNEL_VERSION := 4.19
 TARGET_KERNEL_CONFIG := \
 	vendor/bengal-perf_defconfig \
